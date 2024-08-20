84c54afec21116beb87876168ca10e899193d56c
JacksonSmith43 <jessica444@gmx.at>
2024-08-20 12:16:06 +0200

    Added exit and new word option for multi player. Once a word has been inputed the form disappears.

--- js/multi_player.js
+++ js/multi_player.js
@@ -3,15 +3,31 @@ document.addEventListener("DOMContentLoaded", function () {
     const inputedWord = document.querySelector("input");
     let submitInputedWordButton = document.getElementById("submit-inputed-word-button");
     let outputedWord = document.querySelector(".outputed-word"); // querySelector is used, to only get the first element. 
-
+    let nextWord = document.getElementById("next-word");
+    let exitGame = document.getElementById("exit-game");
 
     submitInputedWordButton.addEventListener("click", function (e) {
         e.preventDefault(); // Because of the form tag, this prevents the page from reloading after it has been sumbitted.
 
         outputedWord.textContent = `Word: ${inputedWord.value}` // e.target is often used, when multiple buttons/inputs have the same name. It determines, which one (event) was triggered.
+
         localStorage.setItem("savedWord", inputedWord.value);
         localStorage.getItem("savedWord");
+
+        document.getElementById("player-one-container").style.display = 'none';
+        document.getElementById("player-two-container").style.display = 'block';
+
     });
 
+
+    nextWord.addEventListener("click", function () {
+        document.getElementById("player-one-container").style.display = 'block';
+        document.getElementById("player-two-container").style.display = 'none';
+    });
+
+
+    exitGame.addEventListener("click", function () {
+        window.location.href = "../index.html";
+    });
 
 });
