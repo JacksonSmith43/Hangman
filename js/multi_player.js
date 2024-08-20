95cc5e061b449f1eeee551008b059e60fc3d21f6
JacksonSmith43 <jessica444@gmx.at>
2024-08-20 11:46:29 +0200

    One can add a word and save it.

--- js/multi_player.js
+++ js/multi_player.js
@@ -1,12 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    let inventedWord = document.getElementById("invented-word");
-    let submitInventedWordButton = document.getElementById("submit-invented-word-button");
-
-    submitInventedWordButton.addEventListener("click", function () {
-        // Add word to game. 
+    const inputedWord = document.querySelector("input");
+    let submitInputedWordButton = document.getElementById("submit-inputed-word-button");
+    let outputedWord = document.querySelector(".outputed-word"); // querySelector is used, to only get the first element. 
 
 
+    submitInputedWordButton.addEventListener("click", function (e) {
+        e.preventDefault(); // Because of the form tag, this prevents the page from reloading after it has been sumbitted.
+
+        outputedWord.textContent = `Word: ${inputedWord.value}` // e.target is often used, when multiple buttons/inputs have the same name. It determines, which one (event) was triggered.
+        localStorage.setItem("savedWord", inputedWord.value);
+        localStorage.getItem("savedWord");
     });
 
 
